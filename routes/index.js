7de88254750fd29cac8690f7f949f7735d1cc229
torgill43 <104460644+torgill43@users.noreply.github.com>
2023-06-07 18:38:20 -0600

    update swagger docs

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
 const router = require("express").Router();
 const { auth } = require("express-openid-connect");
 const dotenv = require("dotenv");
-// const { requiresAuth } = require("express-openid-connect");
-// dotenv.config;
+const { requiresAuth } = require("express-openid-connect");
+dotenv.config;
 
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: process.env.SECRET,
-//   baseURL: process.env.BASE_URL,
-//   clientID: process.env.clientID,
-//   issuerBaseURL: process.env.issuerBaseURL,
-// };
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL,
+};
 
 // auth router attaches /login, /logout, and /callback routes to base url
-// router.use(auth(config));
+router.use(auth(config));
 // req.isAuthenticated is provided from the auth router
-// router.get("/", (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-// });
+router.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
-// router.get("/profile", requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.oidc.user));
-// });
+router.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 // Route for Swagger UI
-// router.use("/", requiresAuth(), require("./swagger"));
-router.use("/", require("./swagger"));
+router.use("/", requiresAuth(), require("./swagger"));
+// router.use("/", require("./swagger"));
 
 // Player Route
-router.use("/baseball-player", require("./playerRoute"));
+router.use("/baseball-player", requiresAuth(), require("./playerRoute"));
 // Team Route
-router.use("/baseball-team", require("./teamRoute"));
+router.use("/baseball-team", requiresAuth(), require("./teamRoute"));
 
 router.get("/", (req, res) => {
   res.send("Sydney Kate Orgill");
